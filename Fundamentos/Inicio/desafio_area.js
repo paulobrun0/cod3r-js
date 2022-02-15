0891fe0b07189cf6bdaeccbd7965a9bb55a416f4
paulobrun0 <paulo.bruno04@hotmail.com>
2022-02-15 13:20:42 -0300

    dados e desafios, fim do modulo de fundamentos

---
+++ Fundamentos/Inicio/desafio_area.js
@@ -0,0 +1,7 @@
+// PI * raio * raio
+
+let raio = 10;
+const PI = 3.14;
+let areaDaCircunferencia = PI * (raio * raio);
+
+console.log("A área da Circunferência é: " + areaDaCircunferencia + " M²");
