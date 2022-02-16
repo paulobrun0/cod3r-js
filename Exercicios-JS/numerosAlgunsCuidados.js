d861fc3cb9e9a9e717f5236331eb9a5e1386a29a
paulobrun0 <paulo.bruno04@hotmail.com>
2022-02-16 13:25:16 -0300

    Iniciando Fundamentos Javascript: comentários, numeros e seus cuidados, variáveis e Constantes

---
+++ Exercicios-JS/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0); // Retorna Infinity
+console.log("10" / 2); // Retorna 5
+console.log("Show" * 2); // Retorna NaN
+console.log(0.1 + 0.7);
+// console.log(10.toString())
+console.log((10.345).toFixed(2));
